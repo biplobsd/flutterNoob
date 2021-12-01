89aa58854d22b49a0c0e27c6c8db5763d205d3da
Biplob Sutradhar <biplobsd11@gmail.com>
2021-12-01 18:01:52 +0600

    Spread Operator

--- lib/main.dart
+++ lib/main.dart
@@ -22,39 +22,19 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) => MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: const Text("SnackBar"),
+            title: const Text("Spread Operator"),
           ),
-          body: Column(
-            children: [
-              Center(
-                child: Builder(
-                  builder: (context) {
-                    return GestureDetector(
-                      onTap: () {
-                        Scaffold.of(context).showSnackBar(
-                          const SnackBar(
-                            duration: Duration(seconds: 1),
-                            content: Text("This is the SnackBar"),
-                          ),
-                        );
-                        isSee();
-                      },
-                      child: Visibility(
-                          child: Image.asset("assets/splash.png"),
-                          visible: isVisible),
-                    );
-                  },
+          body: Center(
+            child: Column(
+              children: [
+                ...Iterable.generate(11).map(
+                  (e) => ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Button $e"),
+                  ),
                 ),
-              ),
-              Visibility(
-                  visible: !isVisible,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      isSee();
-                    },
-                    child: const Text("Resee"),
-                  ))
-            ],
+              ],
+            ),
           ),
         ),
       );
