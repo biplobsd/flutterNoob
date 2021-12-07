ffda8e8995dd1e4da1fea39eb1df8e805b041aae
Biplob Sutradhar <biplobsd11@gmail.com>
2021-12-07 13:47:25 +0600

    WordPairGenerator

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,124 @@
-import 'dart:math';
-
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-  final List<AnimatedIconData> aniIcon = [
-    AnimatedIcons.add_event,
-    AnimatedIcons.arrow_menu,
-    AnimatedIcons.close_menu,
-    AnimatedIcons.ellipsis_search,
-    AnimatedIcons.event_add,
-    AnimatedIcons.home_menu,
-    AnimatedIcons.list_view,
-    AnimatedIcons.menu_arrow,
-    AnimatedIcons.menu_close,
-    AnimatedIcons.pause_play,
-    AnimatedIcons.play_pause,
-    AnimatedIcons.search_ellipsis,
-    AnimatedIcons.view_list,
-    AnimatedIcons.menu_home,
-  ];
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text("AnimatedIcon Button"),
-            centerTitle: true,
-          ),
-          body: Center(
-            child: Wrap(
-              children: [...aniIcon.map((e) => _Buttons(e))],
-            ),
-          ),
-        ),
-      );
-}
-
-class _Buttons extends StatefulWidget {
-  final AnimatedIconData name;
-  const _Buttons(this.name, {Key? key}) : super(key: key);
-
-  @override
-  _ButtonsState createState() => _ButtonsState(name);
-}
-
-class _ButtonsState extends State<_Buttons> with TickerProviderStateMixin {
-  final AnimatedIconData name;
-  late AnimationController _controller;
-  bool isPlaying = false;
-
-  _ButtonsState(this.name);
-
-  @override
-  void initState() {
-    super.initState();
-
-    Random rn = Random();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: rn.nextInt(50) + 700),
-      reverseDuration: Duration(milliseconds: rn.nextInt(50) + 700),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WordPairGenerator',
+      home: const RandomWords(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white, foregroundColor: Colors.black),
+      ),
     );
-
-    _controller.forward().then((_) async {
-      while (true) {
-        await Future.delayed(Duration(seconds: rn.nextInt(2) + 1));
-        _controller.reverse();
-        await Future.delayed(Duration(seconds: rn.nextInt(2) + 1));
-        _controller.forward();
-      }
-    });
   }
+}
 
-  void actionIcon() {
-    setState(() {
-      isPlaying ? _controller.reverse() : _controller.forward();
-      isPlaying = !isPlaying;
-    });
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      // Add lines from here...
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ), // ...to here.
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: Container(
-          color: Colors.lightBlue[50],
-          child: IconButton(
-            highlightColor: Colors.transparent,
-            splashColor: Colors.transparent,
-            onPressed: actionIcon,
-            iconSize: 80,
-            icon: AnimatedIcon(
-              icon: name,
-              progress: _controller,
-              color: Colors.lightBlueAccent,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WordPairGenerator'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
           ),
-        ),
+        ],
       ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(
+              generateWordPairs().take(10),
+            );
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair suggestion) {
+    final alreadySaved = _saved.contains(suggestion);
+    return ListTile(
+      title: Text(
+        suggestion.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(suggestion);
+          } else {
+            _saved.add(suggestion);
+          }
+        });
+      },
     );
   }
 }
